dba1dba7f0661b9642ece8a9088c52a342df960a
Cyberarm <matthewlikesrobots@gmail.com>
2019-10-05 17:11:31 -0500

    Simplify window.h

--- include/window.h
+++ include/window.h
@@ -6,8 +6,4 @@
 #include "stdbool.h"
 #include <Gosu/Window.h>
 
-static mrb_value mrb_gosu_window_initialize(mrb_state *mrb, mrb_value self);
-static mrb_value mrb_gosu_window_show(mrb_state *mrb, mrb_value self);
-static mrb_value mrb_gosu_window_close(mrb_state *mrb, mrb_value self);
-
 void mrb_gosu_window_init(mrb_state *mrb, struct RClass *mrb_gosu);
